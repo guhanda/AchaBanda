fa67766c3d48f9cf7b6bcd72004da55b13c3b416
Bruno Alves <bruno.alves@consulting-for.edenred.com>
2017-01-24 18:59:19 -0200

    concluindo layout do cadastro de instrumentos

--- Front/www/js/controllers/instrumentocontroller.js
+++ Front/www/js/controllers/instrumentocontroller.js
@@ -1,38 +1,64 @@
 angular.module('app').controller('instrumentoCtrl', 
-    ['$scope','InstrumentoFactory','$ionicNavBarDelegate','$location',
-    function ($scope, InstrumentoFactory, $ionicNavBarDelegate, $location) {
+    ['$scope','InstrumentoFactory','$ionicNavBarDelegate','$location','$ionicPopup',
+    function ($scope, InstrumentoFactory, $ionicNavBarDelegate, $location,$ionicPopup) {
 
         var cadastrar = this;
         $ionicNavBarDelegate.showBackButton(false);
         cadastrar.formData = InstrumentoFactory.bindLoginFormData;
         cadastrar.formData.listInstrumentos = [];
+        cadastrar.formData.listInstrumentosDisponiveis = [];
         
-        $scope.ratingsObject = {
-            iconOn: 'ion-ios-star',    //Optional
-            iconOff: 'ion-ios-star-outline',   //Optional
-            iconOnColor: 'rgb(17,193,243)',  //Optional
-            iconOffColor:  'rgb(17,193,243)',    //Optional
-            rating:  2, //Optional
-            minRating:1,    //Optional
-            readOnly: true, //Optional
-            callback: function(rating, index) {    //Mandatory
-            $scope.ratingsCallback(rating, index);
-            }
+        cadastrar.formData.listInstrumentosDisponiveis.push(
+            {id: 0, nome: "GUITARRA"},
+            {id: 1, nome: "BATERIA"},
+            {id: 2, nome: "TECLADO"},
+            {id: 3, nome: "BAIXO"});
+            
+        cadastrar.zerarRatings = function(){
+            $scope.ratingsObject = {
+                iconOn: 'ion-ios-star',    //Optional
+                iconOff: 'ion-ios-star-outline',   //Optional
+                iconOnColor: 'rgb(17,193,243)',  //Optional
+                iconOffColor:  'rgb(17,193,243)',    //Optional
+                rating:  2, //Optional
+                minRating:1,    //Optional
+                readOnly: true, //Optional
+                callback: function(rating, index) {    //Mandatory
+                $scope.ratingsCallback(rating, index);
+                }
+            };
         };
-        
+                     
         $scope.ratingsCallback = function(rating, index) {
             cadastrar.formData.rating = rating;
         };
         
         cadastrar.addInstrumento = function(){
             
+            if(cadastrar.formData.listInstrumentos.length >= 3)
+            {
+                
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Ops!',
+                    template: 'Você já tem 3 instrumentos cadastrados.',
+                    buttons: [
+                        {
+                            text: '<b>Beleza</b>',
+                            type: 'button-calm',
+                        }
+                    ]
+                });
+                
+                return false;
+            }
+            
             cadastrar.formData.listInstrumentos.push({
-                id: cadastrar.formData.listInstrumentos.length,
-                nome: cadastrar.formData.instrumento,
+                id: cadastrar.formData.listInstrumentosDisponiveis[cadastrar.formData.instrumento].id,
+                nome: cadastrar.formData.listInstrumentosDisponiveis[cadastrar.formData.instrumento].nome,
                 rating: cadastrar.formData.rating
             });
 
-            console.log(cadastrar.formData.listInstrumentos);
+            cadastrar.zerarRatings();
         };
 
         cadastrar.removeInstrumento = function(item){
@@ -45,10 +71,23 @@ angular.module('app').controller('instrumentoCtrl',
                 }
             }
         };
+        
+        cadastrar.filterAlreadyAdded = function(item){
+            
+            var retorno = jslinq(cadastrar.formData.listInstrumentos)
+                            .where(function(value){ return value.id == item.id; }).count();
+            
+            return (retorno == 0);
+            //return (cadastrar.formData.listInstrumentos.index(item) == -1);
+        };
 
         cadastrar.cadastrar = function(){
             
             $location.path('/menu.home');
+        }
+        
+        cadastrar.pageLoad = function(){
+            cadastrar.zerarRatings();
         }
     }
 ]);
